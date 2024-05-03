8f116bf6165e5d7294237a9241a449ddb4b67b7e
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-05-03 19:36:43 +0200

    [add] Index, db, products

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/Model/Products.php';
+require_once __DIR__ . '/data/db.php';
+
+// $croccantiniCaneXL = new Products('Royal Canin giant adult', 64.99, 'https://shop-cdn-m.mediazs.com/bilder/7/800/rc_shn_giantadult_mv_1_7.jpg', 'Cani');
+
+// $croccantiniCaneUrinary = new Products('Royal Canin Veterinary Urinary', 21.99, 'https://static.zoomalia.com/prod_img/30709/xl_164fa7cdfad1a5aaf8370ebeda47a1ff1c31553781090.jpg', 'Cani');
+
+// $scatolettaGatti = new Products('Monge Monoprotein Cat Lattina', 33.99, 'https://arcaplanet.vtexassets.com/arquivos/ids/225172/monge-cat-gr-80-sfilaccetti-tacchino-carote.jpg?v=637454750099070000', 'Gatti');
+
+// var_dump($croccantiniCaneXL);
+// var_dump($croccantiniCaneUrinary);
+// var_dump($scatolettaGatti);
+
+var_dump($db);
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  
+</body>
+
+</html>
